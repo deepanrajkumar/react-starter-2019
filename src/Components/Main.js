02fa93f1c640b5ad9f9ad19128f6480a5fcfae51
Deepan Raj Kumar <deepanrr@thoughtworks.com>
2019-11-07 15:12:51 +0530

    updating loader

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -27,13 +27,19 @@ class Main extends Component {
   render() {
     const { response, loading } = this.state;
     return loading ? (
-      <div>Loading....</div>
+      <div>
+        Run the following script
+        <pre>
+          <code>npm run json-server</code>
+        </pre>
+      </div>
     ) : (
       <div>
         <h3 className="new-world">!! Hello world !!</h3>
-        <p>
-          <pre>{JSON.stringify(response, null, 4)}</pre>
-        </p>
+
+        <pre>
+          <code>{JSON.stringify(response, null, 4)}</code>
+        </pre>
       </div>
     );
   }
